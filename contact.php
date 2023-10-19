25de6ebe82ed968f41208c829cb14a7ecd39e1dd
Rush Perera <104535750+Rush-Perera@users.noreply.github.com>
2023-10-19 21:23:00 +0530

    php convert

--- contact.php
+++ contact.php
@@ -1,3 +1,7 @@
+<?php
+include "functions/functions.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- functions/changeParams.php
+++ functions/changeParams.php
@@ -7,4 +7,5 @@ if (isset($_GET['program']) && in_array($_GET['program'], ['dce', 'dcee','deie',
 if (isset($_GET['project']) && in_array($_GET['project'], ['dmmeP1', 'dmmeP2','dmmeP3'])) {
   $_SESSION['project'] = $_GET['project'];
 }
+
 ?>
